11595dbfe2156a3d651f6f9dcf8077de0ff2021e
wanfeng <2460972805@qq.com>
2025-04-10 13:51:36 +0800

    完善

--- src/main/java/com/back/backdata/service/impl/UserServiceImpl.java
+++ src/main/java/com/back/backdata/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.back.backdata.service.impl;
 import com.back.backdata.mapper.UserMapper;
 import com.back.backdata.pojo.User;
 import com.back.backdata.service.UserService;
+import com.back.backdata.utils.Md5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,7 +28,9 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.getUserByStudentId(studentId);
         if (user != null) {
             String newPassword = studentId + "abc";  // 使用学号生成新密码
-            userMapper.updatePasswordByStudentId(studentId, newPassword);
+            // 对新密码进行MD5加密
+            String encryptedPassword = Md5Util.getMD5String(newPassword);
+            userMapper.updatePasswordByStudentId(studentId, encryptedPassword);
         }
     }
 } 
